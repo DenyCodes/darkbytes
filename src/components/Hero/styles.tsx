59d36a9b2b71e03c105cd6a12c87734b17056c47
Denis Oliveira <denisdev3loper@gmail.com>
2025-10-12 10:27:26 -0300

    background hero

--- src/components/Hero/styles.tsx
+++ src/components/Hero/styles.tsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components'
-
+import Hero from '../../images/hero.jpg'
 export const Sect = styled.section`
-  background-color: #f5f5f5;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background:
+    linear-gradient(97deg, rgba(0, 0, 0, 0) -1.38%, rgba(0, 0, 0, 0.83) -1.37%),
+    url(${Hero}) lightgray 50% / cover no-repeat;
   color: #000;
   padding: 4rem 2rem;
   text-align: center;
@@ -13,6 +18,7 @@ export const Sect = styled.section`
   min-height: 80vh;
 
   h1 {
+    text-transform: uppercase;
     font-size: 2rem;
     margin-bottom: 1rem;
     color: #e67e30;
@@ -22,10 +28,17 @@ export const Sect = styled.section`
   }
 
   p {
-    font-size: 1.1rem;
+    color: #fff;
+    font-size: 15px;
     margin-bottom: 2rem;
     max-width: 700px;
     width: 100%;
+    text-transform: uppercase;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    text-align: center;
   }
 
   button {
@@ -49,7 +62,7 @@ export const Sect = styled.section`
       font-size: 2.5rem;
     }
     p {
-      font-size: 1.25rem;
+      font-size: 1rem;
     }
   }
 
@@ -58,11 +71,11 @@ export const Sect = styled.section`
     padding: 6rem 4rem;
 
     h1 {
-      font-size: 3rem;
+      font-size: 2rem;
     }
 
     p {
-      font-size: 1.3rem;
+      font-size: 15px;
     }
 
     button {
